7a45696918cb31d7a58238de7f3c512b331cd58b
yaroslavKonst <yaroslav.konst@yandex.ru>
2022-10-26 04:12:03 +0300

    Added exact solution.

--- Makefile
+++ Makefile
@@ -1,9 +1,17 @@
 .PHONY: compile run
 
+PREC=1.5e-6
+
 compile: program
 
 program: program.c
 	mpicc -Wall -fopenmp -o program program.c -lm
 
 run: compile
-	mpirun -np 4 ./program 1e-5
+	mpirun --use-hwthread-cpus -np 2 ./program $(PREC)
+	mpirun --use-hwthread-cpus -np 3 ./program $(PREC)
+	mpirun --use-hwthread-cpus -np 4 ./program $(PREC)
+	mpirun --use-hwthread-cpus -np 5 ./program $(PREC)
+	mpirun --use-hwthread-cpus -np 6 ./program $(PREC)
+	mpirun --use-hwthread-cpus -np 7 ./program $(PREC)
+	mpirun --use-hwthread-cpus -np 8 ./program $(PREC)
--- program.c
+++ program.c
@@ -4,28 +4,28 @@
 
 #include <mpi.h>
 
-#define POINTS 100
-#define SOLUTION 100e8000
+#define POINTS 1000
+#define SOLUTION 0.06225419868854213
 
 struct point
 {
-	float x;
-	float y;
-	float z;
+	double x;
+	double y;
+	double z;
 };
 
-float f(struct point* p)
+double f(struct point* p)
 {
-	if (p->x * p->x + p->y * p->y + p->z * p->z <= 1.0f) {
+	if (p->x * p->x + p->y * p->y + p->z * p->z <= 1.0) {
 		return sin(p->x * p->x + p->z * p->z) * p->y;
 	} else {
-		return 0.0f;
+		return 0.0;
 	}
 }
 
-float get_random(float min, float range)
+double get_random(double min, double range)
 {
-	return min + (float)rand() * range / (float)RAND_MAX;
+	return min + (double)rand() * range / (double)RAND_MAX;
 }
 
 struct point* generate_points(int count)
@@ -33,25 +33,26 @@ struct point* generate_points(int count)
 	struct point* buffer = malloc(sizeof(struct point) * count);
 
 	for (int idx = 0; idx < count; ++idx) {
-		buffer[idx].x = get_random(0.0f, 1.0f);
-		buffer[idx].y = get_random(0.0f, 1.0f);
-		buffer[idx].z = get_random(0.0f, 1.0f);
+		buffer[idx].x = get_random(0.0, 1.0);
+		buffer[idx].y = get_random(0.0, 1.0);
+		buffer[idx].z = get_random(0.0, 1.0);
 	}
 
 	return buffer;
 }
 
-void master(int rank, int n_proc, float epsilon)
+void master(int rank, int n_proc, double epsilon)
 {
-	float sum = 0;
+	double sum = 0;
 	int n_points = 0;
 
-	int points_per_iter = (n_proc - 1) * POINTS;
+	int points_per_iter_local = POINTS / (n_proc - 1);
+	int points_per_iter = points_per_iter_local * (n_proc - 1);
 
-	int* init_scatter_count = malloc(sizeof(int) * n_proc - 1);
+	int* init_scatter_count = malloc(sizeof(int) * n_proc);
 	int* init_scatter_offset = malloc(sizeof(int) * n_proc);
 
-	int* scatter_count = malloc(sizeof(int) * n_proc - 1);
+	int* scatter_count = malloc(sizeof(int) * n_proc);
 	int* scatter_offset = malloc(sizeof(int) * n_proc);
 
 	init_scatter_count[0] = 0;
@@ -64,17 +65,17 @@ void master(int rank, int n_proc, float epsilon)
 		init_scatter_count[idx] = 1;
 		init_scatter_offset[idx] = 0;
 
-		scatter_count[idx] = POINTS * 3;
-		scatter_offset[idx] = (idx - 1) * POINTS * 3;
+		scatter_count[idx] = points_per_iter_local * 3;
+		scatter_offset[idx] = (idx - 1) * points_per_iter_local * 3;
 	}
 
-	for (int iter = 0; iter < 100; ++iter) {
-		struct point* points_buf = generate_points(points_per_iter);
-		int points = POINTS;
+	struct point* points_buf = generate_points(points_per_iter);
+
+	do {
 		n_points += points_per_iter;
 
 		MPI_Scatterv(
-			&points,
+			&points_per_iter_local,
 			init_scatter_count,
 			init_scatter_offset,
 			MPI_INT,
@@ -88,29 +89,34 @@ void master(int rank, int n_proc, float epsilon)
 			points_buf,
 			scatter_count,
 			scatter_offset,
-			MPI_FLOAT,
+			MPI_DOUBLE,
 			NULL,
 			0,
-			MPI_FLOAT,
+			MPI_DOUBLE,
 			0,
 			MPI_COMM_WORLD);
 
 		free(points_buf);
+		points_buf = generate_points(points_per_iter);
 
-		float value;
-		float zero = 0;
+		double value;
+		double zero = 0;
 
 		MPI_Reduce(
 			&zero,
 			&value,
 			1,
-			MPI_FLOAT,
+			MPI_DOUBLE,
 			MPI_SUM,
 			0,
 			MPI_COMM_WORLD);
 
 		sum += value;
-	}
+
+		//printf("%lf\n", sum / (double)n_points);
+	} while (fabs((sum / (double)n_points) - SOLUTION) >= epsilon);
+
+	free(points_buf);
 
 	int zero = 0;
 
@@ -125,8 +131,10 @@ void master(int rank, int n_proc, float epsilon)
 		0,
 		MPI_COMM_WORLD);
 
-	float result = sum / (float)n_points;
-	printf("%f\n", result);
+	double result = sum / (double)n_points;
+
+	printf("Result = %.17lf\n", result);
+	printf("Error = %.17lf\n", fabs(result - SOLUTION));
 }
 
 void worker(int rank, int n_proc)
@@ -155,16 +163,16 @@ void worker(int rank, int n_proc)
 			NULL,
 			NULL,
 			NULL,
-			MPI_FLOAT,
+			MPI_DOUBLE,
 			buffer,
 			points * 3,
-			MPI_FLOAT,
+			MPI_DOUBLE,
 			0,
 			MPI_COMM_WORLD);
 
-		float sum = 0;
+		double sum = 0;
 
-		#pragma omp parallel for reduction(+:sum)
+		//#pragma omp parallel for reduction(+:sum)
 		for (int idx = 0; idx < points; ++idx) {
 			sum += f(buffer + idx);
 		}
@@ -175,7 +183,7 @@ void worker(int rank, int n_proc)
 			&sum,
 			NULL,
 			1,
-			MPI_FLOAT,
+			MPI_DOUBLE,
 			MPI_SUM,
 			0,
 			MPI_COMM_WORLD);
@@ -191,14 +199,13 @@ int main(int argc, char** argv)
 	int rank;
 	int n_proc;
 
-	float epsilon;
-
-	sscanf(argv[1], "%f", &epsilon);
-
 	MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	if (rank == 0) {
+		double epsilon = atof(argv[1]);
+		printf("e = %.17lf\n", epsilon);
+
 		master(rank, n_proc, epsilon);
 	} else {
 		worker(rank, n_proc);
@@ -217,7 +224,7 @@ int main(int argc, char** argv)
 		MPI_COMM_WORLD);
 
 	if (rank == 0) {
-		printf("%lf\n", total_time);
+		printf("Time = %.17lf\n", total_time);
 	}
 
 	MPI_Finalize();
